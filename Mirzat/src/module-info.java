1288d88108771621ede7e6a93e9c0fdbdf90d2af
Mirzat <mirzatmu@gmail.com>
2019-05-13 19:52:18 -0400

    latest update

--- Mirzat/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author mirzat
- *
- */
-module Mirzat {
-}
---
+++ Mirzat/src/package1/VirginiaBeach.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class VirginiaBeach {
+
+	public static void main(String[] args) {
+		//hello world
+	}
+	
+	
+	
+}
